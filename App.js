8dfbfe03078df507a95dd602b6df5a1168b28b96
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-08-31 11:01:36 +0530

    Initializing the app

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { provider } from "react-redux";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Uber!!</Text>
+      </View>
+    </Provider>
   );
 }
 
