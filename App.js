35d90e58053a36ed304b5fa2b78dddef4ef14774
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-08-31 11:48:18 +0530

    Implementing Redux

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { provider } from "react-redux";
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Uber!!</Text>
       </View>
