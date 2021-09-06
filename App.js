5d1989b83a3f831e7c373c85a17321ba8410e550
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-09-06 09:54:14 +0530

    Building the HomeScreen

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber!!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
