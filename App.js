4fd72f2a01727cc282773bf7b74f8d1010dc4e70
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-09-07 10:59:40 +0530

    Implementing React Native Navigation

--- App.js
+++ App.js
@@ -4,13 +4,32 @@ import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text>Map stuff</Text>
+        </View>
+    )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
