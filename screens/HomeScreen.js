a3c6843388ffe4bb77aafcf8ecc6989d811ada9c
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-09-07 08:58:33 +0530

    Building the NavOptions Component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
@@ -16,6 +17,8 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs"
           }}
         />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
