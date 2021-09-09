9ec009372e60d6af27bda01ebbdaf5acb4fb5772
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-09-09 12:27:59 +0530

    NavigateCard Component final

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env';
@@ -7,6 +13,7 @@ import { useDispatch } from 'react-redux';
 import { setDestination } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/native';
 import NavFavourites from './NavFavourites';
+import { Icon } from 'react-native-elements';
 
 const NavigateCard = () => {
   const dispatch = useDispatch();
@@ -43,7 +50,27 @@ const NavigateCard = () => {
         </View>
 
         <NavFavourites />
+      </View>
 
+      <View
+        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('RideOptionsCard')}
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+          <Icon name='car' type='font-awesome' color='white' size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+          <Icon
+            name='fast-food-outline'
+            type='ionicon'
+            color='black'
+            size={16}
+          />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
